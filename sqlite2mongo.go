5c3575ade067c17433e910e6949dcc3da74ce8d9
Korman <kormanwork@icloud.com>
2018-01-25 17:59:23 +0800

    formatted code

--- sqlite2mongo.go
+++ sqlite2mongo.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"database/sql"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -13,9 +13,6 @@ func main() {
 		fmt.Printf(err.Error())
 	} else {
 		rows, err := db.Query("SELECT * FROM cdsgus where CtfId LIKE '370102199%'")
-		checkErr(err)
-
-		fmt.Println("hehehe")
 
 		for rows.Next() {
 			var name string = ""
@@ -52,60 +49,20 @@ func main() {
 			var version string = ""
 			var id string = ""
 
-			err = rows.Scan(&name, &cardNo, &descriot, &ctfTp,&ctfId,
-				&gender,&birthday,&address,&zip,&dirty,&district1,
-					&district2,&district3,&district4,&district5,&district6,&firstNm,
-						&lastNm,&duty,&mobile,&tel,&fax,&email,
-							&nation,&taste,&education,&company,&cTel,&cAddress,
-								&cZip,&family,&version,&id)
+			err = rows.Scan(&name, &cardNo, &descriot, &ctfTp, &ctfId,
+				&gender, &birthday, &address, &zip, &dirty, &district1,
+				&district2, &district3, &district4, &district5, &district6,
+				&firstNm, &lastNm, &duty, &mobile, &tel, &fax, &email, &nation,
+				&taste, &education, &company, &cTel, &cAddress, &cZip, &family,
+				&version, &id)
 
 			if err != nil {
 				fmt.Println(err)
 			}
 
-			checkErr(err)
 			fmt.Println(name + " " + ctfId + "  " + address + "  " + gender + " " + birthday)
 		}
 	}
 
-	//插入数据
-	//stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
-	//checkErr(err)
-	//
-	//res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
-	//checkErr(err)
-	//
-	//id, err := res.LastInsertId()
-	//checkErr(err)
-	//
-	//fmt.Println(id)
-	////更新数据
-	//stmt, err = db.Prepare("update userinfo set username=? where uid=?")
-	//checkErr(err)
-	//
-	//res, err = stmt.Exec("astaxieupdate", id)
-	//checkErr(err)
-	//
-	//affect, err := res.RowsAffected()
-	//checkErr(err)
-	//
-	//fmt.Println(affect)
-
-	//查询数据
-
-	//删除数据
-	//stmt, err = db.Prepare("delete from userinfo where uid=?")
-	//checkErr(err)
-	//
-	//res, err = stmt.Exec(id)
-	//checkErr(err)
-	//
-	//affect, err = res.RowsAffected()
-	//checkErr(err)
-
 	db.Close()
-}
-
-func checkErr(err error) {
-
 }
